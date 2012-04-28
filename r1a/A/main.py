b8c32ac65c60a5a9728f3fcf99cc2e715a2f1d03
hanasoo <hanasoo@gmail.com>
2012-04-28 20:58:37 +0900

    add code to assist codejam

--- r1a/A/main.py
+++ r1a/A/main.py
@@ -1,55 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+codejam assist
+ @hanasoo
+"""
+
+import optparse
+import os
+import sys
+
 from decimal import *
 
-FILE_NAME = 'sample'
-FILE_NAME = 'A-small-attempt0'
-FILE_NAME = 'A-large'
+class Solver:
+	def __init__(self, case_num, src):
+		self.case_num = case_num
+		self.src = src
 
-IN_FILE = FILE_NAME + '.in'
-OUT_FILE = FILE_NAME + '.out'
+	def option1(self, current, total, probs):
+		c_prob = reduce(lambda x, y: x * y, probs)
+		expect = c_prob * (total - current + 1) + (1 - c_prob) * (total * 2 - current + 2)
+		return expect
 
-f = open(IN_FILE, 'r')
-src = f.read().split('\n')
-f.close()
+	def option2(self, current, total, probs):
+		print '### try go back all'
+		expect = current + total + 1
+		c_prob = probs.pop(0)
+		for i in range(current - 1, 0, -1):
+			print '### try go back %d, %f' % (i, c_prob)
+			c_ex = i * 2 + total - current + 1
+			e = c_prob * c_ex + (1-c_prob) * (c_ex + total + 1)
+			c_prob *= probs.pop(0)
+			expect = min(expect, e)
+		return expect
 
-case_num = int(src.pop(0))
-case_count = 0
-result = ''
+	def option3(self, current, total, probs):
+		expect = total + 2
+		return expect
 
-def option1(current, total, probs):
-	c_prob = reduce(lambda x, y: x * y, probs)
-	expect = c_prob * (total - current + 1) + (1 - c_prob) * (total * 2 - current + 2)
-	return expect
+	def run(self):
+		for i in range(self.case_num):
+			current, total = [int(char) for char in self.src.pop(0).strip().split(' ')]
+			probs = [Decimal(char) for char in self.src.pop(0).strip().split(' ')]
+			ans = min([self.option1(current, total, probs),
+				self.option2(current, total, probs), 
+				self.option3(current, total, probs)])
+			yield i, '%06f' % ans
 
-def option2(current, total, probs):
-	print '### try go back all'
-	expect = current + total + 1
-	c_prob = probs.pop(0)
-	for i in range(current - 1, 0, -1):
-		print '### try go back %d, %f' % (i, c_prob)
-		c_ex = i * 2 + total - current + 1
-		e = c_prob * c_ex + (1-c_prob) * (c_ex + total + 1)
-		c_prob *= probs.pop(0)
-		expect = min(expect, e)
-	return expect
+def main ():
+	# Option parser
+	parser = optparse.OptionParser(usage='%prog [options] input_file')
+	parser.add_option('-o', '--out_file',
+		action = 'store', dest = 'out_file',
+		help = 'Write output filename')
+	parser.add_option('--base_dir',
+		action = 'store', dest = 'base_dir',
+		help = ('Path to base directory '))
+	parser.set_defaults(base_dir = os.path.dirname(os.path.realpath(__file__)))
+	options, args = parser.parse_args()
 
-def option3(current, total, probs):
-	expect = total + 2
-	return expect
+	if not len(args):
+		raise Exception('need input-filename')
 
-print('############ start #################')
-for case_count in range(case_num):
-	current, total = [int(char) for char in src.pop(0).strip().split(' ')]
-	probs = [Decimal(char) for char in src.pop(0).strip().split(' ')]
-	print 'start %s %s %s' % (current, total, probs)
-	ans = min([option1(current, total, probs), option2(current, total, probs), option3(current, total, probs)])
-	ans = 'Case #%d: %06f\n' % (case_count + 1, ans)
-	print ans
-	result += ans
-print('############ end ###################')
+	in_file = args[0]
+	out_file = options.out_file if options.out_file else in_file.replace('.in', '.out')
+	print ('input file: %s' % in_file)
+	print ('output file: %s\n' % out_file)
 
+	# read file
+	f = open(in_file, 'r')
+	src = f.read().split('\n')
+	f.close()
 
-f = open(OUT_FILE, 'w')
-f.write(result)
-f.close()
+	case_num = int(src.pop(0))
+	s = Solver(case_num, src)
+	result = ''
+	for case in s.run():
+		num, ans = case
+		ans = 'Case #{0}: {1}\n'.format(num + 1, ans)
+		print ans
+		result += ans
+
+	# write file
+	f = open(out_file, 'w')
+	f.write(result)
+	f.close()
+
+if __name__ == '__main__':
+	main()
